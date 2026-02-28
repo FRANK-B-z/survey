e76f996ae504bb140dcbccf68d03d4dd81ea2d17
FRANK-B-z <frabanda120@gmail.com>
2026-02-28 08:58:02 +0200

    Refactor buy/sell selection to use radio buttons

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-const buySellSelect = document.getElementById("buySellSelect");
+const buySellRadios = document.querySelectorAll("input[name='buy_sell']");
 const sellingSection = document.getElementById("sellingSection");
 const buyingSection = document.getElementById("buyingSection");
 
-buySellSelect.addEventListener("change", function() {
-
-    if (this.value === "both") {
-        sellingSection.style.display = "block";
-        buyingSection.style.display = "block";
-    }
-
-    else if (this.value === "buy") {
-        sellingSection.style.display = "none";
-        buyingSection.style.display = "block";
-    }
-
-    else {
-        sellingSection.style.display = "none";
-        buyingSection.style.display = "none";
-    }
-
+buySellRadios.forEach(radio => {
+    radio.addEventListener("change", function() {
+        if (this.value === "both") {
+            sellingSection.style.display = "block";
+            buyingSection.style.display = "block";
+        } else if (this.value === "buy") {
+            sellingSection.style.display = "none";
+            buyingSection.style.display = "block";
+        } else {
+            sellingSection.style.display = "none";
+            buyingSection.style.display = "none";
+        }
+    });
 });
 
+const useAppRadios = document.querySelectorAll("input[name='use_app']");
+const businessSection = document.getElementById("businessSection");
+
+useAppRadios.forEach(radio => {
+    radio.addEventListener("change", function() {
+        if (this.value === "yes") {
+            businessSection.style.display = "block";
+        } else {
+            businessSection.style.display = "none";
+        }
+    });
+});
